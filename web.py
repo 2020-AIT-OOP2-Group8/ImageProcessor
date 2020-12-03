b48a2e544ec6be1a3cf1348495820e0d80759933
sk-hdwd <75057834+sk-hdwd@users.noreply.github.com>
2020-12-03 16:59:00 +0900

    12/3 途中経過

--- web.py
+++ web.py
@@ -1,10 +1,37 @@
 from flask import Flask, request, render_template, jsonify
+import os
+
+UPLOAD_FOLDER = './upload_images'
+FILE_TYPE = set(['jpg', 'png'])
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def check_fileType(filename):
+    if '.' in filename and filename.rsplit('.', 1)[1].lower() in FILE_TYPE:
+        check = 1
+    else:
+        check = 0
+
+    return check
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        
+        if file.filename == "":
+            return render_template("image_process.html", result_message="ファイルが選択されていません")
+
+        if check_fileType(file.filename):
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+            return render_template("image_process.html", result_message="アップロードが完了しました")
+        else:
+            return render_template("image_process.html", result_message="この拡張子のファイルは選択できません")
+
     return render_template("image_process.html")
+
 
 if __name__ == "__main__":
     app.run()
