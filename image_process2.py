22c910a44db8d2c62c40e46a35dee4ad8f415f50
hiraiwa0928 <58802704+hiraiwa0928@users.noreply.github.com>
2020-12-06 03:08:58 +0900

    顔を四角で囲む＆モザイクで隠すプログラム
    顔をモザイクで隠すプログラムを追加しました。この画像はstatic/mosaic_output_imagesに格納されます

--- image_process2.py
+++ image_process2.py
@@ -4,7 +4,6 @@ from watchdog.observers import Observer
 
 # ファイルアクセスとスリープのため、osとtimeをインポート
 import os
-from os import listdir
 import time
 
 # 画像処理
@@ -16,21 +15,39 @@ target_dir = 'static/upload_images'
 def makeImage(filename):
 
     imagePath = 'static/upload_images/'+filename # アップロードされた画像のパスを取得
-    outputPath = 'static/face_output_images/face_' + filename # アウトプット先のパスを指定
-    cascade_path = 'cascades/haarcascade_frontalface_default.xml' # 顔を検出してくれるxmlファイルの場所を指定
+    outputPath_face = 'static/face_output_images/face_' + filename # 顔を四角で囲む画像のアウトプット先のパスを指定
+    outputPath_mosaic = 'static/mosaic_output_images/mosaic_' + filename # 顔をモザイクで隠す画像のアウトプット先のパスを指定
+    cascade_path = 'cascades/haarcascade_frontalface_alt2.xml' # 画像処理に必要なxmlファイルの場所を指定
     print(imagePath)
     get_img = cv2.imread(imagePath) # アップロードされた画像を取得
+    gray_img = cv2.imread(imagePath, 0) # get_imgをグレースケール化で保存
 
-    cascade = cv2.CascadeClassifier(cascade_path) # 以下の二行で顔の場所を判別
-    facerect = cascade.detectMultiScale(get_img, scaleFactor=1.1, minNeighbors=1, minSize=(100, 100))
+    rect_img = get_img.copy() # rect_imgにオリジナル画像をコピー
+    mosaic_img = get_img.copy() # mosaic_imgにオリジナル画像をコピー
 
-    if len(facerect) > 0: # 写真の中に顔があった場合
-        for rect in facerect:
-            #矩形描画
-            cv2.rectangle(get_img, tuple(rect[0:2]),tuple(rect[0:2]+rect[2:4]),(255,255,255),8)
-    print(imagePath)
-    cv2.imwrite(outputPath, get_img) # 出力
+    cascade = cv2.CascadeClassifier(cascade_path)
+    faces = cascade.detectMultiScale(gray_img)
+
+    if len(faces) > 0: # 写真の中に顔があった場合
+        for face in faces:
+            x, y, w, h = face
+
+            # 検出した顔の範囲を四角で囲む
+            rect_img = cv2.rectangle(rect_img, (x, y), (x+w, y+h), color=(255, 255, 255), thickness=5)
+            cv2.imwrite(outputPath_face, rect_img)
+
+            # 検出した顔の範囲をモザイクする
+            roi = mosaic_img[y:y+h, x:x+w]
+            roi = cv2.resize(roi, (w//10, h//10))
+            roi = cv2.resize(roi, (w, h), interpolation=cv2.INTER_NEAREST)
+            mosaic_img[y:y+h, x:x+w] = roi 
+    else:
+        cv2.imwrite(outputPath_face, get_img)
+
     print(f'Correct: face_{filename}')
+    
+    cv2.imwrite(outputPath_mosaic, mosaic_img) # 出力
+    print(f'Correct: mosaic_{filename}')
 
 # PatternMatchingEventHandler の継承クラスを作成
 class FileChangeHandler(FileSystemEventHandler):
